e62d9e580a24c0c33b24cdf415300af5bed1cc41
swojeet <swojeetkayastha@gmail.com>
2017-08-15 11:47:13 +0545

    Add routes and methods for site controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,2 +1,13 @@
 class SiteController < ApplicationController
+  def home
+  end
+
+  def blog
+  end
+
+  def about
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'site#home'
+
+  get '/', to: 'site#home'
+  get '/blog', to: 'site#blog'
+  get '/about_me', to: 'site#about'
+  get '/contact', to: 'site#contact'
 end
